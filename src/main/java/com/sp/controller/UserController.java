50e5878929ce1c43ce9b3203a9178fdd279f7145
sreekanth38 <51230190+sreekanth38@users.noreply.github.com>
2021-04-05 18:38:32 +0530

    added Cross Origin annotation

--- src/main/java/com/sp/controller/UserController.java
+++ src/main/java/com/sp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +23,7 @@ public class UserController {
 	@Autowired
 	private UserService service;
 	
-	
+	@CrossOrigin
 	@PostMapping("/registration")
 	private Response<User> createUser(@RequestBody User user)
 	{
